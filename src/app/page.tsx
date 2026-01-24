6077ce2f4b0a4d02312dce02ab891f8b87cd2c42
Atticus Wong <atticuswong@gmail.com>
2026-01-24 03:36:37 -0800

    fix: landing page portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen pt-40 justify-center">
-      <div className="w-1/3 font-bold">
-        Hi my name is Atticus, a developer and writer in the Bay Area.
-      </div>
+			<div className="flex flex-col gap-4 w-1/4">
+				<div className="font-extrabold text-lg">
+					Atticus Wong
+				</div>
+
+				<div className="font-medium">
+					Hi, I am a developer in the Bay Area! I&apos;m currently a student @ UC Davis. I love learning about all things SWE-related. 
+					Recently I&apos;ve been exploring AI Agents and DevOps.
+				</div>
+
+				<div className="font-medium">
+					Here are some articles about some projects I&apos;ve learned:
+				</div>
+				<div className="font-medium">
+					You can read my writing or {" "}
+					<a href="https://github.com/Atticus-Wong" className="underline" target="_blank" rel="noopener noreferrer">code</a>, 
+					or {" "}
+					<a href="https://www.linkedin.com/in/atticus-wong/" className="underline" target="_blank" rel="noopener noreferrer">follow me online</a>. 
+					Reach out if interested.
+				</div>
+			</div>
     </div>
   );
 }
