cf71e40611393d28a4afa284a6736e6ef20e2591
Atticus Wong <atticuswong@gmail.com>
2026-01-24 03:12:20 -0800

    Make main page text bold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="w-1/3 text-center">
-        Hi im this
+    <div className="flex min-h-screen pt-40 justify-center">
+      <div className="w-1/3 font-bold">
+        Hi my name is Atticus, a developer and writer in the Bay Area.
       </div>
     </div>
   );
